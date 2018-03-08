99740a981fd72799b6b106d754bb8be94f00dbb2
e056441 <siva.mannem@mastercard.com>
2018-03-07 18:46:25 -0600

    first commit

---
+++ src/main/java/com/security/example/springbootsecurityoauthclient/SpringbootsecurityoauthclientApplication.java
@@ -0,0 +1,12 @@
+package com.security.example.springbootsecurityoauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsecurityoauthclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsecurityoauthclientApplication.class, args);
+	}
+}
